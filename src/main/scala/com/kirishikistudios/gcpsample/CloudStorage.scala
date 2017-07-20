872b76a5e09e8dda7064f79316700d8b9ef0a2af
chokkoyamada <yamada@kirishikistudios.com>
2017-07-20 10:34:33 +0900

    refactor

--- src/main/scala/com/kirishikistudios/gcpsample/CloudStorage.scala
+++ src/main/scala/com/kirishikistudios/gcpsample/CloudStorage.scala
@@ -1,22 +1,21 @@
 package com.kirishikistudios.gcpsample
 
 import com.google.cloud.storage.{BucketInfo, Storage, StorageOptions}
+
 import scala.collection.JavaConverters._
 
 object CloudStorage extends App {
   val storage: Storage = StorageOptions.getDefaultInstance.getService
   val bucketName = "yamada-test-201707192037"
   val exist = storage.list().iterateAll().iterator().asScala.toList.find(_.getName == bucketName)
-  exist match {
-    case Some(n) => println(s"Bucket $bucketName already exist.")
-    case None =>  createBucket(bucketName)
+  val result = exist match {
+    case Some(_) => s"Bucket $bucketName already exist."
+    case None => createBucket(bucketName)
   }
+  println(result)
 
-  def createBucket(name: String) = {
-    val bucket = storage.create(BucketInfo.of(bucketName))
-    println(s"Bucket $bucketName created.")
+  def createBucket(name: String): String = {
+    storage.create(BucketInfo.of(name))
+    s"Bucket $bucketName created."
   }
 }
-
-
-
